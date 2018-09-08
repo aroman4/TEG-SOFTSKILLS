fbe7dde8c3b9eb026de1d587d2d15ec60b297129
FeliciaJardim <fdcyja@gmail.com>
2018-09-09 00:51:46 +1200

    teg init msg

---
+++ resources/views/index.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    
+    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css"/>
+    <link rel="stylesheet" href="{{asset('css/estilo.css')}}"  />
+
+    <script src="main.js"></script>
+</head>
+<body>
+    <header>
+        <!--<div class="logo">
+        Softskills
+        </div>-->
+        <nav class="menu-estilo-navegacion">
+            <ul class="menu-estilo-principal">
+                <li class="menu-estilo"><a class="menu" href="#"> Softskills</a></li>
+                <li class="menu-estilo"><a class="menu" href="#"> Quienes Somos</a></li>
+                <li class="menu-estilo"><a class="menu" href="#"> Servicios</a></li>
+                <li class="menu-estilo"><a class="menu" href="#"> Staff</a></li>
+                <li class="menu-estilo"><a class="menu" href="#"> Bibliografía</a></li>
+                <li class="menu-estilo"><a class="menu" href="#"> Contactos</a></li>
+            </ul>
+        </nav>
+    </header>
+    <div class="fondo">
+		<img src="5.jpeg">
+	</div>
+	<div class="banner">
+		<h1>Nos dedicamos al desarrollo de las habilidades blandas</h1>
+	</div>
+	<div class="seccion2">
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ipsum nulla, condimentum
+		sed bibendum placerat, interdum eu diam. Aenean elementum vel dui pretium scelerisque. 
+		Nullam feugiat luctus tortor nec hendrerit. </p>
+	</div>
+</body>
+</html>
