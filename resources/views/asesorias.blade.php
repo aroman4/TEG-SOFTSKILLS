349d57807b0de8060bdcb8c39ff133e3b45fea6a
aroman4 <aarb17.91@gmail.com>
2018-09-09 20:43:21 -0400

    Imagenes en laravel

---
+++ resources/views/asesorias.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="{{asset('css/estilo.css')}}">
+</head>
+<body>
+     <header>
+        <div class="logo">
+        Softskills
+        </div>
+        <nav class="menu-estilo-navegacion">
+            <ul class="menu-estilo-principal">
+                <li class="menu-estilo"><a class="menu" href="#"> Quienes Somos</a></li>
+                <li class="menu-estilo"><a class="menu" href="#"> Servicios</a></li>
+                <li class="menu-estilo"><a class="menu" href="#"> Staff</a></li>
+                <li class="menu-estilo"><a class="menu" href="#"> Bibliografía</a></li>
+                <li class="menu-estilo"><a class="menu" href="#"> Contactos</a></li>
+            </ul>
+        </nav>
+    </header>
+	<div class="imagen-fija imagen-fija1">
+		<div class="texto-divImagen">
+			<h1>Asesorías</h1>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ipsum nulla, condimentum
+				sed bibendum placerat, interdum eu diam. Aenean elementum vel dui pretium scelerisque. 
+				Nullam feugiat luctus tortor nec hendrerit
+			</p>
+		</div>
+	</div>
+	<div class="seccion1">
+		<div class="textoSeccion1">
+		<h1>Importancia</h1>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ipsum nulla, condimentum
+		sed bibendum placerat, interdum eu diam. Aenean elementum vel dui pretium scelerisque. 
+		Nullam feugiat luctus tortor nec hendrerit. </p>
+		</div>
+	</div>
+	<div class="imagen-fija imagen-fija2">
+	<div class="texto-divImagen">
+		<h1>Experiencias</h1>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ipsum nulla, condimentum
+			sed bibendum placerat, interdum eu diam. Aenean elementum vel dui pretium scelerisque. 
+			Nullam feugiat luctus tortor nec hendrerit
+		</p>
+	</div>
+	</div>
+	
+	<div class="noesperes">
+        <img src="{{asset('img/2.jpg')}}">
+		<h1>No esperes</h1>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ipsum nulla, condimentum
+		sed bibendum placerat, interdum eu diam. Aenean elementum vel dui pretium scelerisque. 
+		Nullam feugiat luctus tortor nec hendrerit. </p>
+	</div>
+	<footer>
+		<div class="inside-footer">
+		<p>2018 tesis</p>
+		</div>
+	</footer>
+</body>
+</html>
