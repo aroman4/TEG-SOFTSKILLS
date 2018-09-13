edf8bd695adb9a685ab47c236cc9ad36a86665bf
FeliciaJardim <fdcyja@gmail.com>
2018-09-13 22:00:47 +1200

    ultima modificacion

--- resources/views/investigacion.blade.php
+++ resources/views/investigacion.blade.php
@@ -26,7 +26,7 @@
 	</div>
 	<div class="seccion1">
 		<div class="textoSeccion1">
-			<h1>Experiencias</h1>
+			<h1>Importancia</h1>
 			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ipsum nulla, condimentum
 			sed bibendum placerat, interdum eu diam. Aenean elementum vel dui pretium scelerisque. 
 			Nullam feugiat luctus tortor nec hendrerit. </p>
@@ -35,18 +35,20 @@
 	</div>
 	
 	<div class="noesperes">
-		<h1>No esperes</h1>
+		<h1>Experiencias</h1>
 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ipsum nulla, condimentum
 		sed bibendum placerat, interdum eu diam. Aenean elementum vel dui pretium scelerisque. 
 		Nullam feugiat luctus tortor nec hendrerit. </p>
 	</div>
 	<div class="imagen-fija imagen-fija4">
 		<div class="texto-divImagen">
-			<h1>Experiencias</h1>
+			<h1>Como Crear un Proyecto de Investigación</h1>
 			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ipsum nulla, condimentum
 				sed bibendum placerat, interdum eu diam. Aenean elementum vel dui pretium scelerisque. 
 				Nullam feugiat luctus tortor nec hendrerit
-			</p>
+            </p>
+            <br><br>
+            <h1>No esperes mas ser parte de nosotros y demuestra tus conocimientos</h1>
 		</div>
 	</div>
 
