8546fcdef5390619b28fd5a7a63ef2eb7809501d
FeliciaJardim <fdcyja@gmail.com>
2018-09-16 20:52:31 +1200

    solicitud

---
+++ resources/views/solic/solicitud/create.blade.php
@@ -0,0 +1,22 @@
+Solicitud
+
+{!! Form::open(['action' => 'RequestController@store', 'method' => 'POST'])!!}
+    <div class = "form-group">
+        {!! Form::label('nombre_solicitud', 'Nombre de la Solicitud:*  ')!!}
+        {!! Form::text('nombre_solicitud',null,['class'=>'from-control','placeholder'=>'Nombre de la Solicitud','required'])!!}
+    </div>
+    <div class = "form-group">
+            {!! Form::label('actividades', 'Actividades:*'  )!!}
+            {!! Form::text('actividades',null,['class'=>'from-control','placeholder'=>'Actividades a Realizar','required'])!!}
+    </div>
+    <div class = "form-group">
+            {!! Form::label('descripcion', 'Descripción:*' )!!}
+            {!! Form::text('descripcion',null,['class'=>'from-control','placeholder'=>'Escriba aquí','required'])!!}
+    </div>
+    <div class = "form-group">
+            {!! Form::submit('Ajustar Archivo',['class'=>'btn btn-primary'])!!}
+    </div>
+    <div class = "form-group">
+            {!! Form::submit('Enviar Solicitud',['class'=>'btn btn-primary'])!!}
+    </div>
+{!! Form::close()!!}
--- routes/web.php
+++ routes/web.php
@@ -9,4 +9,9 @@
 Route::get('/', 'FrontController@index');
 Route::get('/asesorias', 'FrontController@asesorias');
 Route::get('/investigacion', 'FrontController@investigacion');
+Route::get('/solicitud', 'RequestController@solicitud');
 
+//route de solicitud
+Route::group(['prefix' => 'solic'], function(){
+    Route::resource('solicitud','RequestController');
+});
