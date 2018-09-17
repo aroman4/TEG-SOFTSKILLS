62534bc54bb3b32c8578be08603f1d701569fbca
FeliciaJardim <fdcyja@gmail.com>
2018-09-17 22:41:38 +1200

    prueba

--- resources/views/solic/solicitud/create.blade.php
+++ resources/views/solic/solicitud/create.blade.php
@@ -7,8 +7,16 @@
 </head>
 <body>
         <header>
-              @yield('content')
-         </header>
+                <nav class="menu-estilo-navegacion">
+                        <ul class="menu-estilo-principal">
+                                <li class="menu-estilo"><a class="menu" href="#"> Quienes Somos</a></li>
+                                <li class="menu-estilo"><a class="menu" href="#"> Servicios</a></li>
+                                <li class="menu-estilo"><a class="menu" href="#"> Staff</a></li>
+                                <li class="menu-estilo"><a class="menu" href="#"> Bibliografía</a></li>
+                                <li class="menu-estilo"><a class="menu" href="#"> Contactos</a></li>
+                        </ul>
+                 </nav>
+        </header>
         {!! Form::open(['action' => 'RequestController@store', 'method' => 'POST'])!!}
         <div class = "form-group">
                 {!! Form::label('nombre_solicitud', 'Nombre de la Solicitud:*  ')!!}
