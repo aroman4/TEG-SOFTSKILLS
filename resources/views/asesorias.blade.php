b66945222a8d4870b387693c96e390043256cdde
FeliciaJardim <fdcyja@gmail.com>
2018-09-21 22:37:17 +1200

    arreglos

--- resources/views/asesorias.blade.php
+++ resources/views/asesorias.blade.php
@@ -1,21 +1,5 @@
-<!DOCTYPE html>
-<html>
-<head>
-<link rel="stylesheet" href="{{asset('css/app.css')}}" />
-<link rel="stylesheet" href="{{asset('css/estilo.css')}}">
-</head>
-<body>
-     <header>
-        <nav class="menu-estilo-navegacion">
-            <ul class="menu-estilo-principal">
-                <li class="menu-estilo"><a class="menu" href="#"> Quienes Somos</a></li>
-                <li class="menu-estilo"><a class="menu" href="#"> Servicios</a></li>
-                <li class="menu-estilo"><a class="menu" href="#"> Staff</a></li>
-                <li class="menu-estilo"><a class="menu" href="#"> Bibliografía</a></li>
-                <li class="menu-estilo"><a class="menu" href="#"> Contactos</a></li>
-            </ul>
-        </nav>
-    </header>
+@extends('layouts.plantilla')
+@section('content')
 	<div class="imagen-fija imagen-fija1">
 		<div class="texto-divImagen">
 			<h1>Asesorías</h1>
@@ -50,11 +34,5 @@
 		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ipsum nulla, condimentum
 		sed bibendum placerat, interdum eu diam. Aenean elementum vel dui pretium scelerisque. 
 		Nullam feugiat luctus tortor nec hendrerit. </p>
-	</div>
-	<footer>
-		<div class="inside-footer">
-		<p>2018 tesis</p>
-		</div>
-	</footer>
-</body>
-</html>
+	</div>  
+@endsection
