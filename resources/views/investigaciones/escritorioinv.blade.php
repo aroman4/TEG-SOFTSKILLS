2c418394c69393d75bc9034ee7ba767154f37550
FeliciaJardim <fdcyja@gmail.com>
2018-11-15 22:52:29 -0300

    inves

--- resources/views/investigaciones/escritorioinv.blade.php
+++ resources/views/investigaciones/escritorioinv.blade.php
@@ -2,22 +2,30 @@
 
 @section('content')
 
-<div class="row justify-content-center">
-    <div class="col-md-8">
-            <div class="card-body">
+<div class="col-md-9" style="margin: 0px auto;">
+    <div class="row">
+        <div class="col-md-6">
+            <p>hola2</p>
+            {{--<div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">
+                        @if(Auth::user()->sexo == "Femenino")
+                            <p>Bienvenida {{Auth::user()->nombre ." ". Auth::user()->apellido}}</p>
+                        @else
+                            <p>Bienvenido {{Auth::user()->nombre ." ". Auth::user()->apellido}}</p>
+                        @endif
+                    </div>
+                </div>
+            </div>--}}
+       
+        </div>
+        <div class="col-md-6">
 
-                <div class="col-md-3">
-                    <p>1</p>
-                </div>
-                <div class="col-md-3">
-                    <p>2</p>
-                </div>
-                <div class="col-md-3">
-                    <p>3</p>
-                </div>
-            </div>
-        
+                <p>hola2</p>
+
+        </div>
+       
     </div>
 </div>
-
+        
 @endsection
