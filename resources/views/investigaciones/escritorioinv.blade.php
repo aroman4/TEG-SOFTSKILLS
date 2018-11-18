803beca0d3f19ce81fc1d4e0cdf570af4bdf333f
FeliciaJardim <fdcyja@gmail.com>
2018-11-18 17:05:57 -0300

    inve1

--- resources/views/investigaciones/escritorioinv.blade.php
+++ resources/views/investigaciones/escritorioinv.blade.php
@@ -2,30 +2,33 @@
 
 @section('content')
 
-<div class="col-md-9" style="margin: 0px auto;">
+<div class="col-md-9" style="margin: 0px auto; ">
+    <div class="col-md-12 text-right" style="color:#FFF:black;">
+        @if(Auth::user()->sexo == "Femenino")
+            <h1>Bienvenida {{Auth::user()->nombre ." ". Auth::user()->apellido}}</h1>
+        @else
+            <h1>Bienvenido {{Auth::user()->nombre ." ". Auth::user()->apellido}}</h1>
+        @endif
+    </div>
     <div class="row">
-        <div class="col-md-6">
-            <p>hola2</p>
-            {{--<div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">
-                        @if(Auth::user()->sexo == "Femenino")
-                            <p>Bienvenida {{Auth::user()->nombre ." ". Auth::user()->apellido}}</p>
-                        @else
-                            <p>Bienvenido {{Auth::user()->nombre ." ". Auth::user()->apellido}}</p>
-                        @endif
-                    </div>
-                </div>
-            </div>--}}
-       
+        <div class="col-md-12 list-group-item text-center top-bar">
+            @if(Auth::user()->sexo == "Femenino")
+                <h1>Bienvenida {{Auth::user()->nombre ." ". Auth::user()->apellido}}</h1>
+            @else
+                <h1>Bienvenido {{Auth::user()->nombre ." ". Auth::user()->apellido}}</h1>
+            @endif
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-6" style="">
+            <p>hola2ooooooooooooooooooooooooooooooooooooooooooooooooooooo
+            ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
+            ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
+            oooooooooooooooooooooooooooooooooooooooooooooooooooooooooo</p>
         </div>
         <div class="col-md-6">
-
                 <p>hola2</p>
-
         </div>
-       
     </div>
 </div>
-        
 @endsection
