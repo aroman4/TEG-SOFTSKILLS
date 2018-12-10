1c1e130c8e360f5c5d9e84d9205d88f97b7c0fc8
aroman4 <aarb17.91@gmail.com>
2018-12-09 20:03:35 -0400

    administracion

--- resources/views/admin/usuarios/detalle.blade.php
+++ resources/views/admin/usuarios/detalle.blade.php
@@ -1,32 +1,49 @@
 @extends('layouts.plantilla')
 
 @section('content')
-    <h4>Foto</h4>
-    <div></div>
-    <h4>Nombre y Apellido</h4>
-    <div>{{$usuario->nombre ." ". $usuario->apellido}}</div>
-    <h4>Nombre de usuario</h4>
-    <div>{{$usuario->nombre_usu}}</div>
-    <h4>Sexo</h4>
-    <div>{{$usuario->sexo}}</div>
-    <h4>Email</h4>
-    <div>{{$usuario->email}}</div>
-    <h4>Tipo de usuario</h4>
-    <div>{{$usuario->tipo_usu}}</div>
-    @if($usuario->tipo_usu == "investigador")
-        <h4>Tipo de investigador</h4>
-        <div>{{$usuario->tipo_inv}}</div>
-    @endif
-    <h4>Profesión</h4>
-    <div>{{$usuario->profesion}}</div>
-    <h4>Edad</h4>
-    <div>{{$usuario->edad}}</div>
-    <h4>Teléfono</h4>
-    <div>{{$usuario->telefono}}</div>
-    <h4>Dirección</h4>
-    <div>{{$usuario->direccion}}</div>
-    <h4>Pais</h4>
-    <div>{{$usuario->pais}}</div>
-    <h4>Dirección</h4>
-    <div>{{$usuario->direccion}}</div>
+    <div class="col-md-12 listaQuest">
+            <div class="row separador">
+                <div class="col-md-12 list-group-item text-right top-bar" style="color:white;">
+                        <button style="float:left" onclick="goBack()" class="btn btn-primary boton">Regresar</button>
+                    <h1>(@if($usuario->tipo_inv == "comite")
+                        Miembro Comité
+                    @else
+                        {{$usuario->tipo_usu}}
+                    @endif) {{ $usuario->nombre . " " . $usuario->apellido }}</h1>
+                </div>
+            </div>
+            <div class="row" style="height:70vh">
+                <div class="col-md-6 list-group-item" style="overflow:auto">
+                    <div class="text-center">
+                        <br> 
+                            @if($usuario->imagen != null)
+                                <br><img style="border-radius: 80%; width: 380px;height: 350px;margin: 6px 0;" src="{{asset('imagenperfil/'.$usuario->imagen)}}">
+                             @else
+                             <br><img style="border-radius: 80%; width: 380px;height: 350px;margin: 6px 0;" src="{{asset('imagenperfil/avatarplaceholder.png')}}">
+                            @endif
+                    </div>
+                </div>
+                <div class="col-md-6 list-group-item" style="overflow:auto">
+                    <div class="text-center">
+                        <br>
+                            <li class="list-group-item listaAsesSolic" style="background: #2B3033; color:#FFFFFF">
+                                <h2 style='margin-right:20px'>Perfil de usuario</h2>
+                            </li>
+                        </div>
+                            <li class="list-group-item listaAsesSolic">
+                                <p><b>Nombre:</b> {{$usuario->nombre}}</p>
+                                <p><b>Apellido:</b> {{$usuario->apellido}}</p>
+                                <p><b>Sexo:</b> {{$usuario->sexo}}</p>
+                                <p><b>Edad:</b> {{$usuario->edad}}</p>
+                                <p><b>Dirección:</b> {{$usuario->direccion}}</p>
+                                <p><b>Correo:</b> {{$usuario->email}}</p>
+                                <p><b>Profesión:</b> {{$usuario->profesion}}</p>
+                            </li>
+                            <div style="padding:8px;">                        
+                                <a href="{{route('editarusu',$usuario->id)}}" class="btn btn-success boton1">Editar Pefil</a>
+                                <a href="{{route('usuarios.borrar', $usuario->id)}}" class="btn btn-danger" ><i class="fas fa-user-times"></i> Eliminar</a>
+        {{--                         <a href="#" class="btn btn-danger boton1">Agregar Datos al Perfil </a>
+         --}}                    </div>
+            </div>
+        </div>
 @endsection
